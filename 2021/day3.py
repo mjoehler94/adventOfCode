5998b19f1d02a9c94969276aebcc4dda8d7b3064
Matt Oehler <mjoehler94@gmail.com>
2021-12-08 16:37:54 -0700

    add updates for 3.1

--- 2021/day3.py
+++ 2021/day3.py
@@ -1,20 +1,85 @@
+# day3 ----------
+def binaryToDecimal(binary):
+     
+    binary = int(binary)
+    decimal, i, n = 0, 0, 0
+    while(binary != 0):
+        dec = binary % 10
+        decimal = decimal + dec * pow(2, i)
+        binary = binary//10
+        i += 1
+    return decimal
+
 # read input
 with open("data/day3.txt") as f:
-    vals = [(x.split(" ")[0], int(x.split(' ')[1])) for x in f.readlines()]
+    vals = [x.strip() for x in f.readlines()]
 
 print(vals[:5])
 print()
 
-x = 0
-
+bit_count = dict()
 for v in vals:
-    print(v)
+    for i in range(len(v)):
+        if not bit_count.get(i, False):
+            bit_count[i] = int(v[i])
+        else:
+            bit_count[i] += int(v[i])
+
+
+
+gamma = ""
+epsilon = ""
+for k, v in bit_count.items():
+    if v >= len(vals) / 2:
+        gamma += '1'
+        epsilon += '0'
+    else:
+        gamma += '0'
+        epsilon += '1'
+
+gamma_as_decimal = binaryToDecimal(gamma)
+epsilon_as_decimal = binaryToDecimal(epsilon)
+
 
 # What do you get if you multiply your final horizontal position by your final depth?
 print("Part 1 Answer")
-print(f"")
+print(gamma, gamma_as_decimal)
+print(epsilon, epsilon_as_decimal)
+print(gamma_as_decimal * epsilon_as_decimal)
 print()
 
 
 print("Part 2 Answer")
-print(f"Product of horizontal position {x} and depth {depth} is {x * depth}")
+oxygen = 0
+co2 = 0
+
+
+
+def get_most_common_bit(vals, position):
+    count = 0
+    for v in vals:
+        count += int(v[position])
+    return int(count >= len(vals)/2)
+
+def filter_numbers(vals, position, value):
+    return [v for v in vals if v[position] == value]
+
+# print([get_most_common_bit(vals, x) for x in range(len(vals[0]))])
+
+
+
+def get_rating(vals, most_common=True):
+    temp_vals = vals.copy()
+    position = 0
+    count = 0
+        
+    while(len(temp_vals) > 1):
+        for v in temp_vals:
+            count += int(v[position])
+        if check == '1':
+            pass
+
+
+    return binaryToDecimal(*temp_vals)
+
+
